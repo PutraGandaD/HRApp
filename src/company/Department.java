6ef135ef11daea9f68378a517b3b3add14eb9d9c
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-05-12 00:06:34 +0700

    Import the solutions from Oracle University (not my way of finishing it)
    I think this code from the instructor is over-complicated and not new-developer-friendly type of approach.
    
    But i still include it here, on "master" branch. Go to "putragd" branch to see my way of finishing this case study.

--- src/company/Department.java
+++ src/company/Department.java
@@ -10,19 +10,11 @@ package company;
  */
 public class Department {
     private String dep_name;
-    private Employee[] employee = new Employee[10];
+    private Employee[] employees = new Employee[10];
+    
+    //To indicate array position.
+    private int lastAddedEmployeeIndex = -1;
 
-    public int empCounter() {
-        int counter = 0;
-        
-        for (Employee emp : employee) {
-            if(emp != null) {
-                counter++;
-            }
-        }
-        
-        return counter;
-    }
     
     public Department(String dep_name) {
         this.dep_name = dep_name;
@@ -36,60 +28,111 @@ public class Department {
         this.dep_name = dep_name;
     }
     
-    public void addEmployees(Employee[] new_emp) {
-        if(new_emp.length <= 10) {
-            employee = new_emp;
-        } else {
-            System.out.println("Cannot add employee to Department, because employee is exceeded Department capacity!!!");
+    public void addEmployees(Employee anEmployee) {
+        /*
+           Increment lastAddedEmployeeIndex and add employee to this position in the employees array.
+           When adding new employee to the employees array, consider the length of the array.
+           You can simply stop adding extra employees once you have reached your employee array capacity.
+        */
+        if (lastAddedEmployeeIndex < employees.length) {
+            lastAddedEmployeeIndex++;
+            employees[lastAddedEmployeeIndex] = anEmployee;
         }
     }
     
-    public Employee[] getEmployees() { 
-        Employee[] empData = null;
+    public Employee[] getEmployees() {
+        /*
+        The idea of this algorithm is to return an array of employees
+        that contains the exact number of elements that matches the actual
+        number of employees in this department.
+        This can be calculated by adding one to the lastAddedEmployeeIndex variable.
+        You then need to copy references for relevant employee object from the employees array
+        to this new array and return it to the invoker.
+        This way invoker always gets an array that is already trimmed to exact required size.
+        */
         
-        for(Employee emp : employee) {
-            if(emp != null) {
-                System.out.println(emp);
+        Employee[] actualEmployees = new Employee[lastAddedEmployeeIndex+1];
+        
+        for(int i = 0; i < actualEmployees.length; i++) {
+            actualEmployees[i] = employees[i];
+        }
+        return actualEmployees;
+    }
+    
+    public int getEmployeeCount() {
+        /*
+        Variable lastAddedEmployeeIndex indicates last valid position in the employees array.
+        Because array indexing starts at zero, you need to add one to get the number of elements
+        */
+        return lastAddedEmployeeIndex+1;
+    }
+    
+    //Query method that attempts to locale an employee wirh specific id in this department
+    public Employee getEmployeeByID(int empId) {
+        /*
+        You may reserve a local variable, assign employee object to this variable
+        and break out of the loop, when required employee has been located.
+        
+        Employee result = null;
+        for(Employee emp: employees) {
+            if (emp.getId() == (empId)) {
+                result = emp;
+                break;
             }
         }
+        return result;
         
-        return empData;
+        Infact, result variable will be null if no employee with matching id would be found.
+        
+        Howeverm you may simply return required object once its found and return null if no employee
+        with a given id has been found.
+        */
+        for (Employee emp: employees) {
+            if(emp.getID() == (empId)) {
+                return emp;
+            }
+        }
+        return null;
     }
     
-    public double totalSalary() {
+    public double getTotalSalary() {
+        /*
+        Becase employees array may be partially filled with values, you need to start at position zero
+        and stop at the element indicated by the lastAddedEmployeeIndex variable.
+        Calculate running total value and return the result.
+        */
         double totalSalary = 0.0;
-        
-        for (Employee emp : employee) {
-            if(emp != null) {
-                totalSalary += emp.getSalary();
-            }           
+        for (int i = 0; i <= lastAddedEmployeeIndex; i++) {
+            totalSalary += employees[i].getSalary();
         }
         return totalSalary;
-    }    
-    
-    public double AverageSalary() {
-        double averageSalary;
-     
-        averageSalary = totalSalary() / empCounter();
-        
-        return averageSalary;
     }
     
-    
-    public Employee getEmpbyID(int empId) {
-        Employee empData = null;
-        
-        for (Employee emp : employee) {
-            if (emp != null) {
-                if (empId == emp.getID()) {
-                    empData = emp;
-                    break;
-                }
-            }
+    public double getAverageSalary() {
+        /*
+        To calculate average salary you need to calculate total and divide it by the number of elements.
+        You already have a method that does total calculation.
+        You can find out the number of employees in the array by adding one to the lastAddedEmployeeIndex variable.
+        If there are no employees in the array than you can return zero as the average does not make much sense in this case.
+        */
+        if (lastAddedEmployeeIndex > -1) {
+            return getTotalSalary() / (lastAddedEmployeeIndex+1);
         }
-        
-        return empData;
+        return 0.0;
     }
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+
+    
+    
     
     @Override
     public String toString() {
--- src/company/HRApp.java
+++ src/company/HRApp.java
@@ -33,24 +33,31 @@ public class HRApp {
         Employee e11 = new Employee(133, "Alex", 14.0);
         Employee e12 = new Employee(134, "Alexa", 14.0);
         
-        Employee emp[] = {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11};
+//        Employee emp[] = {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11};
         
         // 4. Create a few employees(above) and add that to Department
-        dep1.addEmployees(emp);
+        dep1.addEmployees(e1);
+        dep1.addEmployees(e2);
+        dep1.addEmployees(new Employee(772, "Jack", 299));
+        
+        Employee[] emps = dep1.getEmployees();
         
         // 5. Locate an employee by their identification number
-        System.out.println(dep1.getEmpbyID(129));
+        System.out.println(dep1.getEmployeeByID(772));
         
-        System.out.println("Total employee in this department = " + dep1.empCounter()); 
+        System.out.println("Total employee in this department = " + dep1.getEmployeeCount()); 
         System.out.println("Employee list = ");
         
         // 6. Get every Employee in that department and print their information
-        System.out.println(dep1.getEmployees());
+        for (Employee emp : emps) {
+            System.out.println("Emp = " + emp);
+        }
+        
         System.out.println("");
         
         // 7-8. Print total salary and Average Salary of that department
-        System.out.println("Total Salary = " + dep1.totalSalary());
-        System.out.println("Average Salary = " + dep1.AverageSalary());
+        System.out.println("Total Salary = " + dep1.getTotalSalary());
+        System.out.println("Average Salary = " + dep1.getAverageSalary());
         
     }
 }
